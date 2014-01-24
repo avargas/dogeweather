335bcce2eb3d7bf98a481eb6a34d83ba033efc9e
Angelo Vargas <angelo@nivler.com>
2014-01-24 11:20:03 -0600

    A few tweaks: no longer need php, client-side GEO & Weather, and retrial in case of errors

--- js/main.js
+++ js/main.js
@@ -1,41 +1,70 @@
-	   $.getJSON('./weather.php', function(data){
+var tries = 0;
+function tryGeo (){
+	if (tries++ > 4) {
+		alert('We tried to fetch your geo, but we failed too many times.');
+		return;
+	}
 
-	   	//$("#degreesCelsius").hide();
-		console.log(data);
+	geoip2.city(fetchWeather, function () { setTimeout(tryGeo, 1000) });
+}
 
-		//set weather id & icon 
-		var id = data.weather[0].id;
-		var icon = data.weather[0].icon;
 
-		$('#weather-id').text(id);
-		$('#weather-icon').text(icon);
+function fetchWeather(geo){
+	var weatherTries = 0;
+	function tryWeather (){
+		if (weatherTries++ > 4) {
+			alert('We tried to fetch your weather data, but it failed too many times.');
+			return;
+		}
 
-		//TESTING 
-		//icon = "01n";
-		//change such doge and sky based on much icon
-		var doge_img = "url(./img/doge/" + icon + ".png)";
-		$('.doge-image').css('background-image', doge_img);
+		var url = 'http://api.openweathermap.org/data/2.5/weather';
+		url += '?lat=' + geo.location.latitude + '&lon=' + geo.location.longitude + '&callback=?';
 
-		var sky_img = "url(./img/sky-img/" + icon + ".png)";
-		$('.bg').css('background-image', sky_img);
+		$.getJSON(url, function(data){
 
-		console.log(icon);
+			console.log(data);
 
-		//get weather description
-		var tempCelcius = data.main.temp - 273.15;
-		var tempFahrenheit = tempCelcius * 9 / 5 + 32;
-		var description = data.weather[0].description;
+			//set weather id & icon 
+			var id = data.weather[0].id;
+			var icon = data.weather[0].icon;
 
-		$('#weather-desc').text("wow " + description);
-		$('#location').text(data.name);
+			$('#weather-id').text(id);
+			$('#weather-icon').text(icon);
 
-		$('#degreesCelsius .number').text(Math.round(tempCelcius));
-		$('#degreesCelsius .cel').text("°C ");
-		$('#degreesFahrenheit').text(Math.round(tempFahrenheit) + "°F");
+			//TESTING 
+			//icon = "01n";
+			//change such doge and sky based on much icon
+			var doge_img = "url(./img/doge/" + icon + ".png)";
+			$('.doge-image').css('background-image', doge_img);
 
-		$(".suchlikes").show();
-		$(".ourinfo").show();
+			var sky_img = "url(./img/sky-img/" + icon + ".png)";
+			$('.bg').css('background-image', sky_img);
 
-		//initialise such doge
-		$($.doge);
-	});
+			console.log(icon);
+
+			//get weather description
+			var tempCelcius = data.main.temp - 273.15;
+			var tempFahrenheit = tempCelcius * 9 / 5 + 32;
+			var description = data.weather[0].description;
+
+			$('#weather-desc').text("wow " + description);
+			$('#location').text(geo.city.names.en);
+
+			$('#degreesCelsius .number').text(Math.round(tempCelcius));
+			$('#degreesCelsius .cel').text("°C ");
+			$('#degreesFahrenheit').text(Math.round(tempFahrenheit) + "°F");
+
+			$(".suchlikes").show();
+			$(".ourinfo").show();
+
+			//initialise such doge
+			$($.doge);
+		}).fail(function(){
+			setTimeout(tryWeather, 1000);
+		});
+	}
+
+	tryWeather();
+}
+
+tryGeo();
